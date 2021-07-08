191bb746f2d5b922db9a5f401c77ea058c7b8814
Marcel Lobão <marcelblobao@gmail.com>
2021-07-08 12:34:44 -0300

    html start test

---
+++ python/model.py
@@ -0,0 +1,10 @@
+class CV:
+    def __init__(self):
+        self.experience = list()
+        self.laguage = list()
+        self.education = list()
+        self.skills = list()
+        self.publications = list()
+        self.projects = list()
+
+
